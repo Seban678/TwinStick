c1f7c9c0cb16d26344ad7ef9e3c7a0d1c6335261
Sebastian Martens <seb.martens@free.fr>
2017-09-06 17:04:56 +0200

    Added replay system, imported character assets

---
+++ Assets/Scripts/ReplaySystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour
+{
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody m_rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Record();
+    }
+
+    void Playback()
+    {
+        m_rigidbody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    void Record()
+    {
+        m_rigidbody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+/// <summary>
+/// A structure for storing time, position and rotation
+/// </summary>
+public struct MyKeyFrame
+{
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 pos, Quaternion rot)
+    {
+        frameTime = time;
+        position = pos;
+        rotation = rot;
+    }
+}
